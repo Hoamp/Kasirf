39b049f4a81269fb0deb7e3fad81dee77da5c280
verapera <116628678+verapera@users.noreply.github.com>
2024-01-24 10:25:21 +0700

    login menu

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Kasir | Login</title>
+  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
+  <link rel="stylesheet" href="../assets/css/styles.min.css" />
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <div
+      class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+      <div class="d-flex align-items-center justify-content-center w-100">
+        <div class="row justify-content-center w-100">
+          <div class="col-md-8 col-lg-6 col-xxl-3">
+            <div class="card mb-0">
+              <div class="card-body">
+                <a href="/home" style="display: flex; align-items: center;" class="text-nowrap logo-img">
+                    <img src="/assets/images/logos/favicon.png" width="35" alt="" />
+                    <h4 class="mt-2" style="margin-left: 10px; font-weight: bold;">APP KASIR SANJAYA</h4>
+                </a>
+                <p class="text-center">Sign-in to start the adventure</p>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                  <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="username" class="form-control" id="username" placeholder="Masukan username...">
+                  </div>
+                  <div class="mb-4">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control" id="password" placeholder="Masukan password...">
+                  </div>
+                  <div class="mb-4">
+                    <label for="level" class="form-label">Level</label>
+                    <select name="level" id="level" class="form-control">
+                        <option value="Administrator">Administrator</option>
+                        <option value="Kasir">Kasir</option>
+                    </select>
+                  </div>
+                  <button type="submit" class="btn btn-primary w-100 py-2 fs-4 mb-4 rounded-2">Sign In</button>
+                </form>      
+                @if(session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @elseif(session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
