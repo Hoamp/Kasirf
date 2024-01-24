da61b9ce74a802b59db96fd4f1eb05e1d352ed15
verapera <116628678+verapera@users.noreply.github.com>
2024-01-24 10:32:07 +0700

    access rights restrictions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\PenggunaController;
@@ -19,21 +20,30 @@ use App\Http\Controllers\PenggunaController;
 Route::get('/', function () {
     return view('index');
 });
+Route::group(['middleware' => 'auth'], function () {
+    Route::controller(PenggunaController::class)->group(function(){
+        Route::get('/dashboard','dashboard')->name('dashboard');
+        Route::get('/pengguna','index')->name('pengguna');
+        Route::post('/tambahpengguna','create')->name('tambahpengguna');
+        Route::delete('/delete/{id}','destroy')->name('delete');
+        Route::get('/editpengguna/{id}', 'edit')->name('editpengguna');
+        Route::put('/updatepengguna/{id}','update')->name('updatepengguna');
+        Route::get('/resetpassword/{id}','reset')->name('resetpassword');
+        Route::get('/login','auth')->name('login');
+    });
 
+    Route::controller(ProdukController::class)->group(function(){
+        Route::get('/produk','index')->name('produk');
+        Route::post('/tambahproduk','store')->name('tambahproduk');
+        Route::delete('/delete_product/{ProdukID}','destroy')->name('delete_product');
 
-
-
-Route::controller(PenggunaController::class)->group(function(){
-    Route::get('/pengguna','index')->name('pengguna');
-    Route::post('/tambahpengguna','create')->name('tambahpengguna');
-    Route::delete('/delete/{id}','destroy')->name('delete');
-    Route::get('/editpengguna/{id}', 'edit')->name('editpengguna');
-    Route::put('/updatepengguna/{id}','update')->name('updatepengguna');
+    });
+    
 });
 
-Route::controller(ProdukController::class)->group(function(){
-    Route::get('/produk','index')->name('produk');
-    Route::post('/tambahproduk','store')->name('tambahproduk');
-    Route::delete('/delete_product/{ProdukID}','destroy')->name('delete_product');
-
+Route::controller(AuthController::class)->group(function(){
+ 
+    Route::get('/login','showLogin')->name('login');
+    Route::post('/login','login');
+    Route::get('/logout','logout')->name('logout');
 });
